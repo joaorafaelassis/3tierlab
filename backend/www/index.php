eec78435261f76156a37b57b91b1a2111ea58f1d
Joao Assis <joaorafaelassis@gmail.com>
2024-02-23 17:02:51 -0300

    Fix cors

--- backend/www/index.php
+++ backend/www/index.php
@@ -1,5 +1,5 @@
 <?php
-header('Access-Control-Allow-Origin: http://172.26.43.128:3000');
+header('Access-Control-Allow-Origin: *');
 $randId=rand(1,20);
 $mysqli = mysqli_connect("database", "db_user", "password!123", "baseteste",3306);
 $result = mysqli_query($mysqli, "SELECT * from times_futebol where id = $randId" );
