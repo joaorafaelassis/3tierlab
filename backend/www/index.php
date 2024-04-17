7f696ddb51d6ea183674f07f355adb52c42e5b81
Joao Assis <joaorafaelassis@gmail.com>
2024-04-17 11:20:15 -0300

    update frontend title

--- backend/www/index.php
+++ backend/www/index.php
@@ -2,7 +2,7 @@
 header('Access-Control-Allow-Origin: *');
 $randId=rand(1,20);
 $mysqli = mysqli_connect("database", "db_user", "password!123", "baseteste",3306);
-$result = mysqli_query($mysqli, "SELECT * from times_futebol where id = $randId" );
+$result = mysqli_query($mysqli, "SELECT time,cidade from times_futebol where id = $randId" );
 $row = mysqli_fetch_assoc($result);
 $jsonResponse = json_encode($row);
 echo $jsonResponse;
